2acc4adf5c0b277a8aa24d55fdd9270835f4fd3e
dvarvarg <dvarvarg@gmail.com>
2024-11-06 11:13:01 +0300

    Добавленые комментарии

--- main_crypto.py
+++ main_crypto.py
@@ -6,25 +6,30 @@ import datetime
 
 
 def update_result_label(event):
+    # Обновляем метку результата (удаляем из нее предыдущие данные)
     result_label.config(text='')
 
 
 def update_cur_label(event):
+    # Получаем полное название валюты (в которую конвертируем) из словаря и обновляем метку
     result_label.config(text='')
     code=combobox_cur.get()
     name=cur[code]
     cur_label.config(text=name)
 
 
-def exchange():
+def exchange(event=None):
     crypt_code=combobox_cript.get()
     cur_code=combobox_cur.get()
     now_dt = datetime.datetime.now().strftime('%d.%m.%Y %X')
 
     if crypt_code and cur_code:
         try:
+            # Отправляем GET-запрос с использованием requests.get()
             response=requests.get(f'https://api.coingecko.com/api/v3/simple/price?ids={crypt_code}&vs_currencies={cur_code}')
-            response.raise_for_status() # Проверка на ошибки
+            # Проверяем успешность запроса (код ответа 200)
+            response.raise_for_status()
+            # Обрабатываем ответ
             json_resp=response.json()
 
             if json_resp:
@@ -40,6 +45,7 @@ def exchange():
 
 sp_cript=['Bitcoin','Ethereum','Ripple','Litecoin','Cardano']
 
+#Словарь кодов валют и их полных названий
 cur= {
     'USD': 'Американский доллар',
     'EUR': 'Евро',
@@ -47,32 +53,37 @@ cur= {
     }
 
 
+# Создание графического интерфейса
 window=Tk()
 window.title('Криптовалюта')
 window.geometry(f'420x200+{window.winfo_screenwidth()//2-210}+{window.winfo_screenheight()//2-100}')
 window.iconbitmap('coins.ico')
 
+# Создание и размещение виджетов
 # лейбл и выпадающий список криптовалют
-Label(text='Выберите криптовалюту: ', font='Arial,10').grid(row=0,column=0,padx=5, pady=10)
+Label(window, text='Выберите криптовалюту: ', font='Arial,10').grid(row=0,column=0,padx=5, pady=10)
 combobox_cript=ttk.Combobox(window,value=sp_cript, font='Arial,10')
 combobox_cript.grid(row=0,column=1,padx=5, pady=2)
 combobox_cript.bind('<<ComboboxSelected>>',update_result_label)
 
 # выпадающий список валют
-Label(text='Конвертировать в: ', font='Arial,10').grid(row=1,column=0,padx=5, pady=2)
-combobox_cur=ttk.Combobox(value=list(cur.keys()), font='Arial,10')
+Label(window, text='Конвертировать в: ', font='Arial,10').grid(row=1,column=0,padx=5, pady=2)
+combobox_cur=ttk.Combobox(window, value=list(cur.keys()), font='Arial,10')
 combobox_cur.grid(row=1,column=1,padx=5, pady=2)
 combobox_cur.bind('<<ComboboxSelected>>',update_cur_label)
 
-cur_label=ttk.Label(font='Arial,10')
+cur_label=ttk.Label(window, font='Arial,10')
 cur_label.grid(row=2,column=1,padx=5, pady=5)
 
 # кнопка вывода
-targ_btn=Button(window, text='Получить курс',command=exchange, font='Arial,10')
+targ_btn=Button(window, text='Получить курс',command=exchange, font='Arial,10',activebackground='#FF6140')
 targ_btn.grid(row=3,column=1,columnspan=1,sticky='ew',padx=5, pady=5)
 
+window.bind('<Return>',exchange)
+
 # лейбл с результатом операции
-result_label=Label(text='Выберите криптовалюту и валюту', font='Arial,10')
+result_label=Label(window, text='Выберите криптовалюту и валюту', font='Arial,10')
 result_label.grid(row=4,column=0, columnspan=2,sticky='ew',padx=5, pady=5)
 
+# Запуск главного цикла окна
 window.mainloop()
